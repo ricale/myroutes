7c0b5ab01d39b99610a1c9deafa01b851e671a23
KIM Kangseong <KIM.kangseong@gmail.com>
2017-12-21 17:07:21 +0900

    Update serializers and views
        - Change HyperlinkedRelatedField to PrimaryKeyRelatedField on serializers
        - Update perform_create on PlaceViewSet

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -2,28 +2,31 @@ from rest_framework import serializers
 from restapi.models import Route, Place, PlaceImage
 from django.contrib.auth.models import User
 
-class RouteSerializer(serializers.HyperlinkedModelSerializer):
+class RouteSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.username')
 
-  places       = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
-  place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-image-detail', read_only=True)
+  # places       = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
+  # place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-image-detail', read_only=True)
+  places       = serializers.PrimaryKeyRelatedField(many=True, queryset=Place.objects.all())
+  place_images = serializers.PrimaryKeyRelatedField(many=True, queryset=PlaceImage.objects.all())
 
   class Meta:
     model = Route
     fields = ('url', 'id', 'owner', 'places', 'place_images', 'name', 'deleted')
 
-class PlaceSerializer(serializers.HyperlinkedModelSerializer):
+class PlaceSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.username')
   route = serializers.ReadOnlyField(source='route.id')
 
-  place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
+  # place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
+  place_images = serializers.PrimaryKeyRelatedField(many=True, queryset=PlaceImage.objects.all())
 
   class Meta:
     model = Place
     fields = ('url', 'id', 'owner', 'route',
               'name', 'address', 'latitude', 'longitude', 'odr', 'place_images')
 
-class PlaceImageSerializer(serializers.HyperlinkedModelSerializer):
+class PlaceImageSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.username')
   route = serializers.ReadOnlyField(source='route.id')
   place = serializers.ReadOnlyField(source='place.id')
@@ -33,10 +36,13 @@ class PlaceImageSerializer(serializers.HyperlinkedModelSerializer):
     fields = ('url', 'id', 'owner', 'route', 'place',
               'original_file_name', 'original_content_type', 'taken_at')
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-  routes       = serializers.HyperlinkedRelatedField(many=True, view_name='route-detail', read_only=True)
-  places       = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
-  place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-image-detail', read_only=True)
+class UserSerializer(serializers.ModelSerializer):
+  # routes       = serializers.HyperlinkedRelatedField(many=True, view_name='route-detail', read_only=True)
+  # places       = serializers.HyperlinkedRelatedField(many=True, view_name='place-detail', read_only=True)
+  # place_images = serializers.HyperlinkedRelatedField(many=True, view_name='place-image-detail', read_only=True)
+  routes       = serializers.PrimaryKeyRelatedField(many=True, queryset=Route.objects.all())
+  places       = serializers.PrimaryKeyRelatedField(many=True, queryset=Place.objects.all())
+  place_images = serializers.PrimaryKeyRelatedField(many=True, queryset=PlaceImage.objects.all())
 
   class Meta:
     model = User
--- restapi/views.py
+++ restapi/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework import generics, permissions, viewsets, status
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 from restapi.models import Route, Place, PlaceImage
 from restapi.serializers import RouteSerializer, PlaceSerializer, PlaceImageSerializer, UserSerializer
 from restapi.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def root(request):
   return render(request, 'root.html')
@@ -22,6 +22,11 @@ class PlaceViewSet(viewsets.ModelViewSet):
   queryset = Place.objects.all()
   serializer_class = PlaceSerializer
   permission_class = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+  def perform_create(self, serializer):
+    route_id = self.request.data['route_id']
+    route = get_object_or_404(Route, id=route_id)
+    serializer.save(owner=self.request.user, route=route)
 
 class PlaceImageViewSet(viewsets.ModelViewSet):
   queryset = PlaceImage.objects.all()
